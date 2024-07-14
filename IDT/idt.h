bbd3a33123a08f7df00e8200fba8641242120e88
ahmed3000xp <113931092+ahmed3000xp@users.noreply.github.com>
2024-07-14 18:40:49 +0300

    Add files via upload

---
+++ IDT/idt.h
@@ -0,0 +1,96 @@
+// Copyright (C) 2024 The RetroFlex OS Project
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "../Headers/stdint.h"
+
+struct IDT_entry_struct{
+    uint16_t base_low;
+    uint16_t sel;
+    uint8_t always0;
+    uint8_t flags;
+    uint16_t base_high;
+}__attribute__((packed));
+
+struct IDT_ptr_struct{
+    uint16_t limit;
+    uint32_t base;
+}__attribute__((packed));
+
+struct InterruptRegisters {
+    uint32_t cr2;
+    uint32_t ds;
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t int_no, err_code;
+    uint32_t eip, cs, eflags, useresp, ss;
+};
+
+void init_IDT();
+void set_IDT_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
+void irq_install_handler (int irq, void (*handler)(struct InterruptRegisters *r));
+void irq_uninstall_handler(int irq);
+void irq_handler(struct InterruptRegisters* regs);
+
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+extern void isr128();
+extern void isr177();
+
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
