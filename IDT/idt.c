7eeb36b76f735543d6d073419ce389dcae2debd7
ahmed3000xp <113931092+ahmed3000xp@users.noreply.github.com>
2024-07-21 22:27:42 +0300

    Add files via upload

--- IDT/idt.c
+++ IDT/idt.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 The RetroFlex OS Project
+// Copyright (C) 2024 Ahmed
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
